ec2ab654261728551e7b2f8c7768568cf27e93c4
SeyaKen <75517657+SeyaKen@users.noreply.github.com>
2022-08-06 11:28:06 +0900

    first commit second

---
+++ lib/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              'You have pushed the button this many times:',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:jugyourogu/home_page.dart';
+
+class MainPage extends StatefulWidget {
+  int currenttab;
+  MainPage({super.key, required this.currenttab});
+
+  @override
+  _MainPageState createState() => _MainPageState(currenttab);
+}
+
+class _MainPageState extends State<MainPage> {
+  int currenttab;
+  _MainPageState(this.currenttab);
+  String? uid;
+
+  getMyUserUid() async {
+    setState(() {});
+  }
+
+  onScreenLoaded() async {
+    await getMyUserUid();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    onScreenLoaded();
+    setState(() {});
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) {
+            setState(() {
+              currenttab = index;
+            });
+          },
+          currentIndex: currenttab,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.black,
+          backgroundColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                size: 30,
+              ),
+              label: 'ホーム',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.account_circle,
+                size: 30,
+              ),
+              label: 'プロフィール',
+            ),
+          ]),
+      body: IndexedStack(
+        index: currenttab,
+        children: const [
+          HomePage(),
+          HomePage(),
+        ],
+      ),
+    );
+  }
+}
