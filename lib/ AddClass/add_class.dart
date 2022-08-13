dfa794b9bb4ffa053c11ff9a63a2d27461ece7ff
SeyaKen <75517657+SeyaKen@users.noreply.github.com>
2022-08-13 15:04:19 +0900

    first second third commit

--- lib/ AddClass/add_class.dart
+++ lib/ AddClass/add_class.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:jugyourogu/main_page.dart';
 
@@ -10,7 +11,25 @@ class AddClass extends StatefulWidget {
 
 class _AddClassState extends State<AddClass> {
   final _formKey = GlobalKey<FormState>();
-  String? ClassName;
+  String? ClassName0;
+  double _kItemExtent = 32.0;
+
+  void _showDialog(Widget child) {
+    showCupertinoModalPopup<void>(
+        context: context,
+        builder: (BuildContext context) => Container(
+              height: 250,
+              padding: const EdgeInsets.only(top: 0),
+              margin: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom,
+              ),
+              color: CupertinoColors.systemBackground.resolveFrom(context),
+              child: SafeArea(
+                top: false,
+                child: child,
+              ),
+            ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,64 +49,134 @@ class _AddClassState extends State<AddClass> {
       ),
       body: SingleChildScrollView(
         child: GestureDetector(
-            onTap: () {
-              final FocusScopeNode currentScope = FocusScope.of(context);
-              if (!currentScope.hasPrimaryFocus && currentScope.hasFocus) {
-                FocusManager.instance.primaryFocus!.unfocus();
-              }
-            },
-            child: Form(
+          onTap: () {
+            final FocusScopeNode currentScope = FocusScope.of(context);
+            if (!currentScope.hasPrimaryFocus && currentScope.hasFocus) {
+              FocusManager.instance.primaryFocus!.unfocus();
+            }
+          },
+          child: Form(
               key: _formKey,
               child: Container(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  children: [
-                  Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-
-                    children: [
-                      Row(
-                        children: const [
-                          Text(
-                            '授業名',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: title_list.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            const SizedBox(
+                              height: 25,
                             ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.9,
-                        child: TextFormField(
-                          style: const TextStyle(color: Colors.white),
-                          validator: (val) => val!.isEmpty ? '' : null,
-                          onChanged: (val) {
-                            setState(() => ClassName = val);
-                          },
-                          decoration: const InputDecoration(
-                            fillColor: Color(0xff333333),
-                            filled: true,
-                            hintText: '授業名',
-                            hintStyle: TextStyle(color: Colors.grey),
-                            contentPadding: EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 13),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.transparent,
-                              ),
+                            Row(
+                              children: [
+                                Text(
+                                  title_list[index],
+                                  style: const TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
                             ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ]),
-              ),
-            )),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            index == 0 || index == 1 || index == 2
+                                ? SizedBox(
+                                    width:
+                                        MediaQuery.of(context).size.width * 0.9,
+                                    child: TextFormField(
+                                      style:
+                                          const TextStyle(color: Colors.white),
+                                      validator: (val) => val!.isEmpty
+                                          ? '正確に${title_value[index]}を入力してください。'
+                                          : null,
+                                      onChanged: (val) {
+                                        setState(
+                                            () => title_value[index] = val);
+                                      },
+                                      decoration: InputDecoration(
+                                        fillColor: const Color(0xff333333),
+                                        filled: true,
+                                        hintText: title_list[index],
+                                        hintStyle:
+                                            const TextStyle(color: Colors.grey),
+                                        contentPadding:
+                                            const EdgeInsets.symmetric(
+                                                horizontal: 10, vertical: 13),
+                                        focusedBorder: const OutlineInputBorder(
+                                          borderSide: BorderSide(
+                                            color: Colors.transparent,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                : SizedBox(
+                                  width: MediaQuery.of(context).size.width *
+                                      0.9,
+                                  
+                                  child: TextFormField(
+                                    onTap: () {
+                                      _showDialog(
+                                        CupertinoPicker(
+                                          magnification: 1.22,
+                                          squeeze: 1.2,
+                                          useMagnifier: true,
+                                          itemExtent: _kItemExtent,
+                                          // This is called when selected item is changed.
+                                          onSelectedItemChanged:
+                                              (int selectedItem) {
+                                            setState(() {
+                                              title_list[index] = selectedItem;
+                                            });
+                                          },
+                                          children: List<Widget>.generate(
+                                              title_list[index].length,
+                                              (int indexx) {
+                                            return Center(
+                                              child: Text(
+                                                title_list[index][indexx],
+                                              ),
+                                            );
+                                          }),
+                                        ),
+                                      );
+                                    },
+                                    readOnly: true,
+                                    style: const TextStyle(
+                                        color: Colors.white),
+                                    validator: (val) => val!.isEmpty
+                                        ? '正確に${title_value[index]}を入力してください。'
+                                        : null,
+                                    onChanged: (val) {
+                                      setState(
+                                          () => title_value[index] = val);
+                                    },
+                                    decoration: InputDecoration(
+                                      fillColor: const Color(0xff333333),
+                                      filled: true,
+                                      hintText: title_list[index],
+                                      hintStyle: const TextStyle(
+                                          color: Colors.grey),
+                                      contentPadding:
+                                          const EdgeInsets.symmetric(
+                                              horizontal: 10, vertical: 13),
+                                      focusedBorder:
+                                          const OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                          color: Colors.transparent,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                          ]);
+                    }),
+              )),
+        ),
       ),
       floatingActionButton: ClipRRect(
         borderRadius: BorderRadius.circular(5),
@@ -116,3 +205,25 @@ class _AddClassState extends State<AddClass> {
     );
   }
 }
+
+List title_list = [
+  '授業名',
+  '教授/講師名',
+  '学部',
+  '曜日・時限1',
+  '曜日・時限2',
+];
+
+String? ClassName0;
+String? ClassName1;
+String? ClassName2;
+String? ClassName3;
+String? ClassName4;
+
+List title_value = [
+  ClassName0,
+  ClassName1,
+  ClassName2,
+  ClassName3,
+  ClassName4,
+];
